ad099d8a2447c15fb232ce180b0e0b3ea0fcd08d
dozddd <polinahandogina86@gmail.com>
2025-10-29 23:27:21 +0500

    theme change and reg

---
+++ index.js
@@ -0,0 +1,22 @@
+// Код для смены темы
+let btn = document.querySelector(".my_btn");
+btn.addEventListener("click", btn_click);
+
+function btn_click() {
+    document.querySelector("body").classList.toggle("body_style");
+}
+
+// Код для модального окна регистрации
+let modal = document.querySelector(".reg-modal");
+
+let btn_reg = document.querySelector(".btn-reg");
+
+let span = document.querySelector(".reg-modal__close");
+
+btn_reg.addEventListener("click", () => {
+    modal.classList.add("block");
+});
+
+span.addEventListener("click", () => {
+    modal.classList.remove("block");
+});
